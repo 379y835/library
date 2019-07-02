bdef0e15daed38b451998df2b366ab1c26cdbaf2
xiaowang <379y835@sina.com>
2019-07-02 09:03:22 +0800

    update 1 file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const userRouter=require('./routes/user.js');
 var app=express();
-
-
-
+app.listen(8080);
+app.use( express.static('public') );
+app.use( bodyParser.urlencoded({
+  extended:false
+}));
+app.use('/user',userRouter);
